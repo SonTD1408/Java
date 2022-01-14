a8c630f136401d29ff22f39c168d25973bf24f29
unknown <tadinhson1999@gmail.com>
2022-01-14 14:14:50 +0700

    custom annotation

---
+++ CustomAnnotation/fieldAnnotation/AnnotationExample.java
@@ -0,0 +1,25 @@
+package fieldAnnotation;
+
+import java.lang.reflect.Field;
+import java.util.Date;
+
+public class AnnotationExample {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Annotation Example");
+        User usr = new User();
+        usr.setEmail("sontd@gmail.com");
+        usr.setName("Son");
+        usr.setId(112);
+        usr.setCreated(new Date());
+
+        for (Field field: usr.getClass().getDeclaredFields()){
+            DBField dbField = field.getAnnotation(DBField.class);
+            System.out.println("Field name: "+dbField.name());
+
+            field.setAccessible(true);
+            Object value = field.get(usr);
+            System.out.println("field value: "+dbField.type());
+            System.out.println("Is primary key: "+dbField.type());
+        }
+    }
+}
---
+++ CustomAnnotation/fieldAnnotation/DBField.java
@@ -0,0 +1,13 @@
+package fieldAnnotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.FIELD)
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DBField{
+    String name();
+    Class<?> type();
+    boolean isPrimaryKey() default false;
+}
